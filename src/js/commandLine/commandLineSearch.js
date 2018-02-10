93a7f8a079fd06d1461f39d449cab30750b5b887
akado117 <akaidan@mtu.edu>
2018-02-09 19:03:10 -0500

    figured out how to past mock out external classes with mock implementations. test to make sure all functions are called within searchForWords finished

--- src/js/commandLine/commandLineSearch.js
+++ src/js/commandLine/commandLineSearch.js
@@ -4,4 +4,8 @@ import WordSearch from '../helperClasses/wordSearch';
 
 export function searchForWords(inputString) {
     const wordSearch = new WordSearch(inputString);
+
+    const wordObjects = wordSearch.findWordsInWordGrid(wordSearch.getWordSearchData());
+    if (!wordObjects) return console.warn('Please format your string as follows csv,words\ng,r,\ni,d ');
+    wordSearch.buildOutputCoordString(wordObjects);
 }
--- src/js/commandLine/commandLineSearch.test.js
+++ src/js/commandLine/commandLineSearch.test.js
@@ -1,12 +1,40 @@
 import { searchForWords } from './commandLineSearch';
+import WordSearch from '../helperClasses/wordSearch';
+const mockGetWordSearchData = jest.fn();
+const mockFindWordsInWordGrid = jest.fn().mockReturnValue('anything');
+const mockBuildOutputCoordString = jest.fn();
+
+jest.mock('../helperClasses/wordSearch', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            getWordSearchData: mockGetWordSearchData,
+            findWordsInWordGrid: mockFindWordsInWordGrid,
+            buildOutputCoordString: mockBuildOutputCoordString,
+        };
+        // Now we can track calls to playSoundFile
+    });
+});
 
 describe('commandLineSearch' , () => {
+   beforeEach(() => {
+       WordSearch.mockClear();
+       mockGetWordSearchData.mockClear();
+       mockFindWordsInWordGrid.mockClear();
+       mockBuildOutputCoordString.mockClear();
+   });
    describe('searchForWords', () => {
        it('should not crash if invalid data fed in', () => {
            searchForWords('holy toledo');
            searchForWords(123);
            searchForWords({});
-           expect(searchForWords()).toBe(undefined);
-       })
+           //expect(searchForWords()).toBe(undefined);
+       });
+       it('should call functions from WordSearch in correct order', () => {
+           searchForWords('word,are\nf,u,n\nn,s,u\nc,h,!');
+           expect(WordSearch).toHaveBeenCalledTimes(1);
+           expect(mockGetWordSearchData).toHaveBeenCalledTimes(1);
+           expect(mockFindWordsInWordGrid).toHaveBeenCalledTimes(1);
+           expect(mockBuildOutputCoordString).toHaveBeenCalledTimes(1);
+       });
    })
 });
